36fdd48d47bed5fb5fb26c632a9802fbaab24638
riteshpatel-dev <riteshpatel.rl46@gmail.com>
2024-11-16 12:20:05 +0000

    Datatypes

---
+++ 1-Basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JavaScript code as newer version
+
+// alert(3+3) // we are using node.js , not browser so it can't be executed 
+
+// code readablity should be high 
+
+let name = "Ritesh" // string * use ""
+
+let age = 18 // number 
+
+let isLoggedIn = false //boolean => true/false
+
+let state = null 
+
+// null => standalone 
+// undefined => value not assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Ritesh");
