1081fd2649668b4384368d30152c3b252ae732b2
riteshpatel-dev <riteshpatel.rl46@gmail.com>
2024-11-17 05:07:29 +0000

    Operations

--- 1-Basics/3_conversionOperation.js
+++ 1-Basics/3_conversionOperation.js
@@ -25,6 +25,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ******************** Operations *********************
+
+let value = 3
+
+// let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //raised to power
+// console.log(2/3);  // divison
+// console.log(2%3); // remainder
+
+let str1 = "hello"
+let str2 = "ritesh"
+let str3 = str1 + str2
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/#sec-type-conversion
